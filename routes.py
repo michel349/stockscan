8e88d56db1e1d79fe8407c744485c887fe38b815
michel349 <73477534+michel349@users.noreply.github.com>
2026-04-08 11:16:48 +0200

    Update routes.py

--- routes.py
+++ routes.py
@@ -5,6 +5,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email import encoders
+from mail import envoyer_mail_commande
 
 from config import (
     DESTINATIONS, DEST_COLORS_HEX,
